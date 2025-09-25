44f86dcd3647a8d6251df04f9ea85d05e254f6c4
Jakub Skála <skalaj24@spstrutnov.cz>
2025-09-25 09:17:30 +0200

    Start Piskvorky
    Tvoření mřížky

--- solution/Projekty1/Program.cs
+++ solution/Projekty1/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Projekty1
+﻿using System;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        char[,] board = new char[3, 3];
+
+        for (int i = 0; i < 3; i++)
         {
-            Console.WriteLine("Hello, World!");
+            for (int j = 0; j < 3; j++)
+            {
+                board[i, j] = '.';
+            }
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            for(int j = 0;j < 3; j++)
+            {
+                Console.WriteLine(board[i, j] + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
