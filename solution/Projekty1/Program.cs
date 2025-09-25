8848ec9730055ab4071f8beac0e43b06e7c13396
Jakub Skála <skalaj24@spstrutnov.cz>
2025-09-25 08:34:47 +0200

    Zahájení projektů

---
+++ solution/Projekty1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Projekty1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
