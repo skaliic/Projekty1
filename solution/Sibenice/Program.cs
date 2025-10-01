9f576749c2c0a091433ca98018035ad2b042f66f
Jakub Skála <skalaj24@spstrutnov.cz>
2025-10-01 10:41:56 +0200

    Šibenice

--- solution/Sibenice/Program.cs
+++ solution/Sibenice/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace Sibenice
+﻿using System;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        string[] words = { "kolo", "počítač", "škola", "hudba", "python", "pes", "fotbal", "auto" };
+        Random rand = new Random();
+        string wordToGuess = words[rand.Next(words.Length)].ToLower();
+
+        char[] guessedWord = new char[wordToGuess.Length];
+        for (int i = 0; i < guessedWord.Length; i++)
+            guessedWord[i] = '_';
+
+        int wrongGuesses = 0;
+        int maxWrongGuesses = 6;
+
+        while (wrongGuesses < maxWrongGuesses && new string(guessedWord) != wordToGuess)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Clear();
+            Console.WriteLine("Šibenice");
+            Console.WriteLine($"Slovo: {string.Join(" ", guessedWord)}");
+            Console.WriteLine($"Chybné pokusy: {wrongGuesses}/{maxWrongGuesses}");
+            Console.Write("Hádej písmeno: ");
+            char guess = Char.ToLower(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+
+            bool correct = false;
+            for (int i = 0; i < wordToGuess.Length; i++)
+            {
+                if (wordToGuess[i] == guess)
+                {
+                    guessedWord[i] = guess;
+                    correct = true;
+                }
+            }
+
+            if (!correct)
+                wrongGuesses++;
         }
+
+        Console.Clear();
+        if (new string(guessedWord) == wordToGuess)
+            Console.WriteLine($"Gratulace! Uhodl jsi slovo: {wordToGuess}");
+        else
+            Console.WriteLine($"Prohrál jsi! Slovo bylo: {wordToGuess}");
     }
 }
+
