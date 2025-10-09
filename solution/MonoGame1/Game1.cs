9db597a96fb6e26801cc9a61718210fcdb33d0a9
Jakub Skála <skalaj24@spstrutnov.cz>
2025-10-09 09:27:51 +0200

    Příprava čtverců

--- solution/MonoGame1/Game1.cs
+++ solution/MonoGame1/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace MonoGame1
 {
@@ -8,6 +9,9 @@ namespace MonoGame1
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        private Texture2D _squareTexture;
+        private List<Player> players;
 
         public Game1()
         {
@@ -20,6 +24,16 @@ namespace MonoGame1
         {
             // TODO: Add your initialization logic here
 
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 600;
+            _graphics.ApplyChanges();
+
+            players = new List<Player>
+            {
+                new Player(new Vector2(100, 100), Color.Red, Keys.W, Keys.S, Keys.A, Keys.D),
+                new Player(new Vector2(400, 300), Color.Blue, Keys.Up, Keys.Down, Keys.Left, Keys.Right)
+            };
+
             base.Initialize();
         }
 
@@ -27,23 +41,32 @@ namespace MonoGame1
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            _squareTexture = new Texture2D(GraphicsDevice, 1, 1);
+            _squareTexture.SetData(new[] { Color.White });
+
+
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
-            // TODO: Add your update logic here
-
+            foreach (var player in players)
+                player.Update(players);
             base.Update(gameTime);
+
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.White);
 
+            _spriteBatch.Begin();
+            foreach (var player in players)
+                player.Draw(_spriteBatch, _squareTexture);
+            _spriteBatch.End();
+            
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
